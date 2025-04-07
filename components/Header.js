58f2160ad427622ff01e3cc2dccc1da10af2831d
Jiang Wenjian <jwenjian.me@gmail.com>
2025-04-07 18:05:52 +0800

    nav bar

--- components/Header.js
+++ components/Header.js
@@ -7,19 +7,19 @@ export default function Header({navBarTitle, fullWidth}) {
     <header className={styles.header}>
       <div className={styles.logo}>
         <Link href="/">
-          <Image src="/logo.png" alt="readzn.com Logo" width={40} height={40}/>
+          <Image src="/logo.png" alt="start.1link.fun Logo" width={40} height={40}/>
         </Link>
         <div className={styles.titleContainer}>
-          <h1>{navBarTitle || "阅读指南"}</h1>
+          <h1>{navBarTitle || "1Link 导航"}</h1>
           <span className={styles.domain}>start.1link.fun</span>
-          <span className={styles.slogan}>人工维护的高质量站点导航</span>
+          <span className={styles.slogan}>纯人工维护的高质量导航站点</span>
         </div>
 
       </div>
       <nav className={styles.nav}>
         <ul>
           <li><Link title="Home" href="/">首页</Link></li>
-          <li><Link title="科技周刊" href="https://1link.fun/" target='_blank'>科技周刊</Link></li>
+          <li><Link title="Technical Weekly" href="https://1link.fun/" target='_blank'>科技周刊</Link></li>
           <li><Link title="Donate Support" href="/donate-support" target='_blank'>捐赠</Link></li>
           <li><Link title="Contact Us" href="/contact">关于我们</Link></li>
         </ul>
